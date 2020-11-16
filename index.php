32e34c5dde95a307da00037047957824b4621450
Marco <fadum.marco@gmail.com>
2020-11-16 10:43:39 +0100

    Login HTML erstellt

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+
+
+
+
+
+?>
+
+<!doctype html>
+<html lang="de">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/login.css" crossorigin="anonymous">
+    <link href="css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
+
+    <title>Hello, world!</title>
+</head>
+<body>
+
+<h1 class="text-center pt-lg-5">E-Banking Application Ultimate</h1>
+
+
+
+
+<div class="wrapper fadeInDown">
+    <div id="formContent">
+        <!-- Tabs Titles -->
+
+        <!-- Icon -->
+        <div class="fadeIn first" >
+            <img src="img/1200px-User_font_awesome.svg.png" id="icon" alt="User Icon"/>
+        </div>
+
+        <!-- Login Form -->
+        <form>
+            <input type="text" id="login" class="fadeIn second" name="login" placeholder="login">
+            <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
+            <input type="submit" class="fadeIn fourth" value="Log In">
+        </form>
+
+        <!-- Remind Passowrd -->
+        <div id="formFooter">
+            <a class="underlineHover" href="#">Forgot Password?</a>
+        </div>
+
+    </div>
+</div>
+
+
+
+
+</body>
+</html>
